6aad684898ab9e8ba53cdb0646a0b8d3eab2611d
Bram Si <si.thutun.2412@gmail.com>
2026-08-13 11:34:16 -0400

    chore: delete unnecessary comments

--- number_guess.sh
+++ number_guess.sh
@@ -7,6 +7,7 @@ GAME_START () {
   then
     USERID=$1
   fi
+
   # generate random number
   SECRET_NUMBER=$((RANDOM % 1000 + 1))
 
@@ -22,15 +23,9 @@ GAME_START () {
 
     # add number_of_guesses
     NUMBER_OF_GUESSES=1
-    #ADD_NUMBER_GUESSED=$($PSQL "UPDATE games SET number_of_guesses=$NUMBER_OF_GUESSES + 1 WHERE game_id=$GAMEID;")
 
-    # if input is a valid integer
-   
-  #    # if the number is greater than 1000
-  #    if [[ $USERGUESS < 1001 ]]
-  #    then  
+    # if input is a valid integer   
     # check if it match with the secret_number
-    #SECRET_NUMBER=$($PSQL "SELECT secret_number FROM games WHERE game_id=$GAMEID;")
     while [[ $USERGUESS -ne $SECRET_NUMBER ]];
     do  
       ((NUMBER_OF_GUESSES++))      
@@ -62,24 +57,16 @@ GAME_START () {
       fi
     done
 
-        # if match
-        # update number_of_guesses
-        UPDATE_NUMBER_OF_GUESSES=$($PSQL "UPDATE games SET number_of_guesses=$NUMBER_OF_GUESSES WHERE game_id=$GAMEID;")
-        # update games_played
-        UPDATE_GAMES_PLAYED=$($PSQL "UPDATE users SET games_played=(SELECT COUNT(user_id) FROM games WHERE user_id=$USERID) WHERE user_id=$USERID;")
-        # update best_game
-        UPDATE_BEST_GAME=$($PSQL "UPDATE users SET best_game=(SELECT number_of_guesses FROM games WHERE user_id = $USERID ORDER BY number_of_guesses ASC LIMIT 1) WHERE USER_id=$USERID;")
-#        BEST_GAMEID=$($PSQL "SELECT game_id FROM games WHERE user_id = $USERID ORDER BY number_of_guesses ASC LIMIT 1;")        
-        
-        # get number_of_guesses
-        #NUMBER_OF_GUESSES=$($PSQL "SELECT number_of_guesses FROM games WHERE game_id=$GAMEID;")
-        echo "You guessed it in $NUMBER_OF_GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
-        # END GAME
-  #    # if input is greater than 1000
-  #    else
-  #      GUESS_CHECK "The number is larger than 1000, guess again:" $GAMEID
-  #    fi
-
+    # if match
+    # update number_of_guesses
+    UPDATE_NUMBER_OF_GUESSES=$($PSQL "UPDATE games SET number_of_guesses=$NUMBER_OF_GUESSES WHERE game_id=$GAMEID;")
+    # update games_played
+    UPDATE_GAMES_PLAYED=$($PSQL "UPDATE users SET games_played=(SELECT COUNT(user_id) FROM games WHERE user_id=$USERID) WHERE user_id=$USERID;")
+    # update best_game
+    UPDATE_BEST_GAME=$($PSQL "UPDATE users SET best_game=(SELECT number_of_guesses FROM games WHERE user_id = $USERID ORDER BY number_of_guesses ASC LIMIT 1) WHERE USER_id=$USERID;")
+    
+    echo "You guessed it in $NUMBER_OF_GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
+    # END GAME
   fi
 }
 
@@ -87,14 +74,8 @@ GAME_START () {
 echo "Enter your username:"
 read USERNAME
 
-#if [[ $USERNAME =~ ^[A-Za-z]{0,22}$ ]]
-#then
-#  ADD_USER $USERNAME
-  # check if it is an old user
+# check if it is an old user
 USERID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME';")
-#GAMES_PLAYED=$($PSQL "SELECT games_played FROM users WHERE user_id = $USERID";)
-#BEST_GAME=$($PSQL "SELECT best_game FROM users WHERE user_id= $USERID";)
-
   if [[ -z $USERID ]]
   # if new user
   then
@@ -114,37 +95,3 @@ USERID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME';")
     echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
     GAME_START $USERID
   fi
-#fi
-
-
-#
-#ADD_USER () {
-#  if  [[ $1 ]]
-#  then
-#    USERNAME=$1
-#  fi
-#  # check if it is an old user
-#  USERID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME';")
-#  if [[ -z $USERID ]]
-#  # if new user
-#  then
-#    # add new user to db
-#    ADD_USER=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME');")
-#    if [[ $ADD_USER == "INSERT 0 1" ]]
-#    then
-#      NEW_USERID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME';")
-#      echo "Welcome, $USERNAME! It looks like this is your first time here."
-#      #GAME_START $NEW_USERID
-#    fi
-#  else
-#    # if an old user
-#    # get games_played, best_game
-#    GAMES_PLAYED=$($PSQL "SELECT games_played FROM users WHERE user_id = $USERID;")
-#    BEST_GAME=$($PSQL "SELECT best_game FROM users WHERE user_id= $USERID;")
-#    echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
-#    #GAME_START $USERID
-#  fi
-#}
-
-
-
